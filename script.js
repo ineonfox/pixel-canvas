3989fa7c4c4764dc97b31f1a88388e4d0d269cf6
ineonfox <61496409+ineonfox@users.noreply.github.com>
2026-03-24 23:33:30 +0100

    Add function to change the color of each cell
    After user hovers over a cell, it adds a background color to it with
    0.1 opacity. After every subsequent hover, it adds +0.1 opacity
    until the cell reaches full opacity. After that, it stays fully colored.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const defaultGridSize = 16;
+const defaultGridSize = 50;
 const canvas = document.querySelector('.canvas');
 createGrid(defaultGridSize);
 
 function createGrid(size) {
     let canvasSize = canvas.offsetWidth;
     let cellSize = canvasSize / size;
-    console.log(canvasSize);
 
     for(let i = 0; i < size * size; i++) {
         let cell = document.createElement('div');
@@ -13,5 +12,22 @@ function createGrid(size) {
         cell.style.width = `${cellSize}px`;
         cell.style.height = `${cellSize}px`;
         canvas.appendChild(cell);
+
+        cell.addEventListener('mouseover', changeCellColor);
     }
+}
+
+function changeCellColor(evt) {
+    let currentOpacity = evt.target.style.backgroundColor.split(',');
+    if (!currentOpacity) {
+        evt.target.style.backgroundColor = 'rgb(0, 0, 0, 0.1)';
+    }
+    else {
+        // when cell is fully colored, opacity value is not reported, therefore .length = 3
+        if (currentOpacity.length >= 4)
+        currentOpacity = currentOpacity[3].substring(1,4);
+        evt.target.style.backgroundColor = `rgb(0, 0, 0, ${Number(currentOpacity) + 0.1})`;
+    }
+    console.log(evt.target.style.backgroundColor);
+    
 }
