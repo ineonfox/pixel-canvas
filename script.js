71a5a67cebe7685c0307b36052d94bba0a3fae4d
ineonfox <61496409+ineonfox@users.noreply.github.com>
2026-03-24 23:15:23 +0100

    Create a 16x16 grid in JS by default

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const defaultGridSize = 16;
 const canvas = document.querySelector('.canvas');
+createGrid(defaultGridSize);
+
+function createGrid(size) {
+    let canvasSize = canvas.offsetWidth;
+    let cellSize = canvasSize / size;
+    console.log(canvasSize);
+
+    for(let i = 0; i < size * size; i++) {
+        let cell = document.createElement('div');
+        cell.style.outline = 'rgb(190, 190, 190) solid 1px';
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+        canvas.appendChild(cell);
+    }
+}
